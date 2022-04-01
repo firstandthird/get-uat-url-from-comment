ef8e3ebfb02bb5d1dc21c1a53adc4e423e1c4677
Troy Whiteley <24781+dawnerd@users.noreply.github.com>
2022-04-01 22:31:46 +0000

    Task to find first matching comment

---
+++ index.js
@@ -0,0 +1,29 @@
+import { getInput, info, setOutput } from "@actions/core";
+import { GitHub, context } from "@actions/github";
+
+async function Runner() {
+  const issue = getInput("issue");
+  const token = getInput("github-token");
+  const searchDomain = getInput("search-domain");
+
+  info(`Looking for comments that contain urls matching: ${searchDomain}`);
+
+  const octokit = new GitHub(token);
+
+  const { data: comments } = await octokit.issues.listComments({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    issue_number: issue,
+    per_page: 100,
+  });
+
+  const matchingComment = comments
+    .map((comment) => comment.body)
+    .find((comment) => comment.includes(searchDomain));
+
+  const [url] = matchingComment.match(/(http?:\/\/[^\s]+)/gi);
+
+  setOutput("url", url ?? "");
+}
+
+Runner();
